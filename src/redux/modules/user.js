8b01dbfaa18a35099b3b73418c4a7d4412d4426b
losajjang <losajjang@naver.com>
2022-04-21 00:44:30 +0900

    feat: LoginCheck 기능 추가

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -3,16 +3,19 @@ import { produce } from "immer";
 import axios from "axios";
 import jwt_decode from "jwt-decode";
 import Cookies from "universal-cookie";
+import api from "../../shared/api";
 
 const cookies = new Cookies();
 
 //actions
+const LOGIN = "user/LOGIN";
 const LOG_OUT = "LOG_OUT";
 const GET_USER = "GET_USER";
 const SET_USER = "SET_USER";
 
 //action creators
-const logout = createAction(LOG_OUT, () => ({}));
+const setLogin = createAction(LOGIN, (user) => ({ user }));
+const logout = createAction(LOG_OUT, (user) => ({ user }));
 const getUser = createAction(GET_USER, (user) => ({ user }));
 const setUser = createAction(SET_USER, (user) => ({ user }));
 
@@ -25,8 +28,8 @@ const initialState = {
 //middleware
 const signupDB = (userId, password, confirmpassword, intro) => {
   return function (dispatch, getState, { history }) {
-    axios
-      .post("https://sparta-hs.shop/api/users", {
+    api
+      .post("/api/users", {
         userId: userId,
         password: password,
         confirmpassword: confirmpassword,
@@ -48,13 +51,13 @@ const signupDB = (userId, password, confirmpassword, intro) => {
 };
 const loginDB = (userId, password) => {
   return function (dispatch, getState, { history }) {
-    axios
-      .post("https://sparta-hs.shop/api/users/login", {
+    api
+      .post("/api/users/login", {
         userId: userId,
         password: password,
       })
       .then((res) => {
-        console.log(res)
+        console.log(res);
         // console.log(res.data.response.UserInfo.userCode);
         dispatch(
           setUser({
@@ -66,7 +69,7 @@ const loginDB = (userId, password) => {
         console.log(accessToken);
         cookies.set("myJWT", accessToken, { path: "/" });
         cookies.set("userCode", userCode, { path: "/" });
-        window.alert(`"${userId}"님 환영합니다`)
+        window.alert(`"${userId}"님 환영합니다`);
         history.replace("/");
         window.location.reload();
       })
@@ -83,12 +86,34 @@ const logoutDB = () => {
     cookies.remove("isLogin");
     cookies.remove("userCode");
     cookies.remove("myJWT");
+    window.alert("로그아웃되었습니다");
+  };
+};
+
+const loginCheckDB = () => {
+  return function (dispatch, getState, { history }) {
+    const userId = cookies.get("userCode");
+    const tokenCheck = document.cookie;
+    console.log(userId);
+    console.log(tokenCheck);
+    if (tokenCheck) {
+      dispatch(setUser({ userId: userId }));
+    } else {
+      dispatch(logout());
+      window.alert("로그인을 다시 해주세요");
+      history.replace("/entrance");
+    }
   };
 };
 
 //reducer
 export default handleActions(
   {
+    [LOGIN]: (state, action) =>
+      produce(state, (draft) => {
+        draft.user = action.payload.user;
+        draft.isLogin = true;
+      }),
     [SET_USER]: (state, action) =>
       produce(state, (draft) => {
         cookies.set("isLogin", "success");
@@ -112,6 +137,7 @@ const actionCreators = {
   signupDB,
   loginDB,
   logoutDB,
+  loginCheckDB,
 };
 
 export { actionCreators };
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 //import { Provider } from 'react-redux';
 import styled from "styled-components";
@@ -7,6 +7,8 @@ import styled from "styled-components";
 import { BrowserRouter, Route } from "react-router-dom";
 import { ConnectedRouter } from "connected-react-router";
 import { history } from "../redux/configureStore";
+import { useDispatch } from "react-redux";
+import { actionCreators } from "../redux/modules/user";
 
 //페이지 임포트
 import LoginModal from "../components/LoginModal";
@@ -15,16 +17,20 @@ import Main from "../pages/Main";
 import Entrance from "../pages/Entrance";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(actionCreators.loginCheckDB());
+  }, []);
   return (
     <React.Fragment>
-      <div className="App">
-        <ConnectedRouter history={history}>
+      <ConnectedRouter history={history}>
+        <div className="App">
           <Route path="/" exact component={Main} />
           <Route path="/entrance" exact component={Entrance} />
           {/* <Route path="/entrance/login" exact component={LoginModal} />
           <Route path="/entrance/signup" exact component={SignupModal} /> */}
-        </ConnectedRouter>
-      </div>
+        </div>
+      </ConnectedRouter>
     </React.Fragment>
   );
 }
